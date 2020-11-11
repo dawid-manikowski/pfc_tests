94292d7349470e07b09f0969429cf87870b9f7ed
czoksu <42738429+dawid-manikowski@users.noreply.github.com>
2020-11-11 12:26:57 +0100

    Update main.py

--- python_test/main.py
+++ python_test/main.py
@@ -3,6 +3,7 @@ from time import sleep
 def handler(robot_event):
   print(robot_event)
   print("Hello PowerFarm Cloudx2!")
+  print("HABABABA")
   for i in range(100):
     sleep(1)
     print(f"I've been running for {i} seconds.")
